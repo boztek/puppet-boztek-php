31d362d2944817b11a346b3244a0f5cdca72c24d
Boris Gordon <boris@boztek.net>
2013-02-22 21:48:21 +1100

    Attempt to build cgi and FPM together

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,44 @@
-class php {
-	
+class php($sapi = "cgi") {
+	require homebrew
+
+	exec { 'brew tap josegonzalez/php':
+		creates => "${homebrew::config::tapsdir}/josegonzalez-php", 
+		before => Exec['php53-cgi'],
+	}
+	exec { 'brew tap homebrew/dupes':
+		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
+		before => Package['zlib'],
+	}
+	package { 'zlib': }
+
+	exec { 'php53-cgi':
+		require => Package['zlib'],
+		command => 'brew install php53 --with-cgi --with-gmp',
+		creates => "${homebrew::config::installdir}/bin/php-cgi",
+	}
+
+	exec { 'php53-cgi-bin':
+		require => Exec['php53-cgi'], 
+		command => "cp ${homebrew::config::installdir}/bin/php-cgi ${boxen::config::home}/bin",
+		creates => "${boxen::config::home}/bin/php-cgi",
+	}
+
+	exec { 'brew uninstall php53': }
+
+	package { 'php53-fpm':
+		name => 'php53',
+		require => Package['zlib'],
+		install_options => [
+			'--with-fpm',
+			'--with-gmp',
+			'--with-tidy',
+		],
+	}
+
+	include php::pear
+
+	Exec['php53-cgi'] -> Exec['php53-cgi-bin'] -> Exec['brew uninstall php53'] -> Package['php53-fpm']
+}
+
+class php::pear {
 }
