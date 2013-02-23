7ae45b83d0e14ef98f536d435117f4ac1f4a232a
Boris Gordon <boris@boztek.net>
2013-02-23 20:06:53 +1100

    Retreat! Install FPM only for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,4 @@
-class php($sapi = "cgi") {
-	notice("Installing PHP")
+class php {
 	require homebrew
 
 	exec { 'brew tap josegonzalez/php':
@@ -13,35 +12,11 @@ class php($sapi = "cgi") {
 
 	package { 'zlib': }
 
-	exec { 'php53-cgi-prepare':
-		command => 'brew uninstall php53 --with-cgi',
-		onlyif  => 'test -d /opt/boxen/homebrew/Cellar/php',
-		before  => Package['php53'],
-	}
-
 	package { 'php53':
 		require => Package['zlib'],
 		install_options => [
-			'--with-cgi',
+			'--with-fpm',
 			'--with-gmp',
 		],
 	}
-
-	exec { 'php53-cgi-bin':
-		command => "cp ${homebrew::config::installdir}/Cellar/php53/5.3.21/bin/php-cgi ${boxen::config::home}/bin",
-		creates => "${boxen::config::home}/bin/php-cgi",
-		onlyif  => 'test -d ${homebrew::config::installdir}/Cellar/php53/5.3.21/bin/php-cgi',
-	}
-
-	exec { 'php53-cgi-cleanup':
-		command => 'brew uninstall php53 --with-cgi',
-		onlyif  => 'test -d ${homebrew::config::installdir}/Cellar/php53/5.3.21/bin/php-cgi',
-	}
-
-	package { 'php53 --with-fpm':
-		require => Package['zlib'],
-		install_options => ['--with-gmp'],
-	}
-
-	Package['php53'] -> Exec['php53-cgi-bin'] -> Exec['php53-cgi-cleanup'] -> Package['php53 --with-fpm']
 }
