77b521103cc807a12e344d8684ed224e02646bb8
Boris Gordon <boris@boztek.net>
2013-03-06 10:05:36 +1100

    Revert "Remove zlib dependency for now"
    This reverts commit f96a2712e878d07b1906f2a632850384a8e9cbd2.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,15 @@ class php {
 		creates => "${homebrew::config::tapsdir}/josegonzalez-php", 
 		before => Package['php53'],
 	}
+	exec { 'brew tap homebrew/dupes':
+		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
+		before => Package['zlib'],
+	}
+
+	package { 'zlib': }
 
 	package { 'php53':
+		require => Package['zlib'],
 		install_options => [
 			'--with-fpm',
 			'--with-gmp',
