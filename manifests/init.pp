4c21c2dfdbe2c5b907b31f4f6b1bee96a4f19e8a
Boris Gordon <boris@boztek.net>
2013-03-06 10:15:23 +1100

    Try pulling out zlib

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
 class php {
-	require homebrew
+	include homebrew
 
 	exec { 'brew tap josegonzalez/php':
 		creates => "${homebrew::config::tapsdir}/josegonzalez-php", 
 		before => Package['php53'],
 	}
-	exec { 'brew tap homebrew/dupes':
-		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
-		before => Package['zlib'],
-	}
-
-	package { 'zlib': }
+#	exec { 'brew tap homebrew/dupes':
+#		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
+#	}
+#	package { 'zlib':
+#		require => Exec['brew tap homebrew/dupes'],
+#	}
 
 	package { 'php53':
-		require => Package['zlib'],
+		# require => Package['zlib'],
 		install_options => [
 			'--with-fpm',
 			'--with-gmp',
