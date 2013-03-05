f96a2712e878d07b1906f2a632850384a8e9cbd2
Boris Gordon <boris@boztek.net>
2013-03-06 09:01:38 +1100

    Remove zlib dependency for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,15 +5,8 @@ class php {
 		creates => "${homebrew::config::tapsdir}/josegonzalez-php", 
 		before => Package['php53'],
 	}
-	exec { 'brew tap homebrew/dupes':
-		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
-		before => Package['zlib'],
-	}
-
-	package { 'zlib': }
 
 	package { 'php53':
-		require => Package['zlib'],
 		install_options => [
 			'--with-fpm',
 			'--with-gmp',
