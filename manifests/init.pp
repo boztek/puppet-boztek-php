02d176c542dddeb1a7474eb4a183bb550a4f60e3
Boris Gordon <boris@boztek.net>
2013-03-08 22:13:36 +1100

    PHP module uses tap defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,32 @@
 class php {
-	require homebrew
 
-	exec { 'brew tap josegonzalez/php':
-		creates => "${homebrew::config::tapsdir}/josegonzalez-php", 
-		before => Package['php53'],
-	}
-	exec { 'brew tap homebrew/dupes':
-		creates => "${homebrew::config::tapsdir}/homebrew-dupes",
-		before => Package['zlib'],
-	}
+  pnx::homebrew::tap { 'homebrew/dupes': }
+  package { 'zlib':
+    require => Pnx::Homebrew::Tap['homebrew/dupes'],
+  }
 
-	package { 'zlib': }
+  pnx::homebrew::tap { 'josegonzalez/php': }
 
-	package { 'php53':
-		require => Package['zlib'],
-		install_options => [
-			'--with-fpm',
-			'--with-gmp',
-		],
-	}
+  package { 'php53':
+    ensure => present,
+    require => [
+      Pnx::Homebrew::Tap['josegonzalez/php'],
+      Package['zlib'],
+    ],
+    install_options => [
+      '--with-fpm',
+      '--with-gmp',
+    ],
+  }
+
+  file { '/Library/LaunchDaemons/dev.php53.plist':
+    content => template('php/dev.php53.plist.erb'),
+    group   => 'wheel',
+    notify  => Service['dev.php53'],
+    owner   => 'root'
+  }
+
+  service { 'dev.php53':
+    ensure  => running,
+  }
 }
